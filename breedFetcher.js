c75ade2d743be5caebea59da13cfcc3d6ddbdbf7
amyfranses <amyfranses@gmail.com>
2022-02-24 22:28:28 +0000

    create fetchBreedDescription function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,22 @@
 const request = require("request");
 const fs = require("fs");
 
-const args = process.argv.slice(2);
-let breedname = args[0];
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        console.log("Error");
+        return;
+      }
+      const data = JSON.parse(body);
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breedname}`,
-  (error, response, body) => {
-    if (error) {
-      console.log("Error");
-      return;
+      if (!data.length) {
+        console.log("No Breed");
+        return;
+      }
+      console.log(`Breedname is ${breedName}: ${data[0]["description"]}`);
     }
-    const data = JSON.parse(body);
-
-    if (!data.length) {
-      console.log("No Breed");
-      return;
-    }
-    console.log(`Breedname is ${breedname}: ${data[0]["description"]}`);
-  }
-);
+  );
+};
+module.exports = { fetchBreedDescription };
