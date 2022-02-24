69d140f16fbfb816acca5bf7f90be3c4f0b79fb7
amyfranses <amyfranses@gmail.com>
2022-02-24 22:18:57 +0000

    test edge cases, refactor code

--- breedFetcher.js
+++ breedFetcher.js
@@ -7,12 +7,16 @@ let breedname = args[0];
 request(
   `https://api.thecatapi.com/v1/breeds/search?q=${breedname}`,
   (error, response, body) => {
-    fs.writeFile("https://api.thecatapi.com/v1/breeds", body, (error) => {
-      if (!error) {
-        return;
-      }
-      const data = JSON.parse(body);
-      console.log(`Breedname is ${breedname}: ${data[0]["description"]}`);
-    });
+    if (error) {
+      console.log("Error");
+      return;
+    }
+    const data = JSON.parse(body);
+
+    if (!data.length) {
+      console.log("No Breed");
+      return;
+    }
+    console.log(`Breedname is ${breedname}: ${data[0]["description"]}`);
   }
 );
